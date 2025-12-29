5954024617790c427cc6f6caa44e5a387d5dcc12
Johannes Steltzer <johannes.steltzer@bwi.de>
2025-12-29 20:14:18 +0100

    added properties joinedAt and leftAt

--- lib/data/player.dart
+++ lib/data/player.dart
@@ -1,12 +1,28 @@
 class Player {
   String name;
   int rating;
+  final int joinedAt;
+  int? leftAt;
 
-  Player({required this.name, required this.rating});
+  Player({required this.name, required this.rating, int? joinedAt})
+    : joinedAt = joinedAt ?? 0;
 
-  Map<String, dynamic> toJson() => {'name': name, 'rating': rating};
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    if (rating != 0) 'rating': rating,
+    if (joinedAt != 0) 'joinedAt': joinedAt,
+    if (leftAt != null) 'leftAt': leftAt,
+  };
 
   factory Player.fromJson(Map<String, dynamic> json) {
-    return Player(name: json['name'], rating: json['rating']);
+    var p = Player(
+      name: json['name'],
+      rating: json['rating'] ?? 0,
+      joinedAt: json['joinedAt'] ?? 0,
+    );
+    if (json['leftAt'] != null) {
+      p.leftAt = json['leftAt'];
+    }
+    return p;
   }
 }
